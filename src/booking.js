3b6256cbbcce77d542da3409abc30505cf92399f
David <david.gomez.redondo.dev@gmail.com>
2021-08-13 09:09:45 +0200

    greedy algorithm description

--- src/booking.js
+++ src/booking.js
@@ -2,3 +2,24 @@
 export default function booking ( some ) {
   console.log(some)
 }
+
+/*
+Referencia --> https://www.wextensible.com/temas/voraces/
+
+function voraz (conjunto) {
+    
+  let resultado = crear(conjunto);
+    
+  while ( !esSolucion(resultado) && !esVacio(conjunto) ) {
+    
+    let candidato = seleccionar(conjunto);
+
+    if (esCompletable(resultado, candidato)) {
+      
+      resultado = guardar(resultado, candidato);
+    }
+  }
+  
+  return resultado;
+}
+*/
